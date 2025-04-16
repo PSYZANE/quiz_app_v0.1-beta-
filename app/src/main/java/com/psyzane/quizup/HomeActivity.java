b62e70ff4dfe03e40907f90de1a8cb59d31efa8c
PSYZANE <adityabudakoti3333@gmail.com>
2025-04-16 12:19:16 +0530

    dynamic layout added
    dynamically adding the questions to the layout

--- app/src/main/java/com/psyzane/quizup/HomeActivity.java
+++ app/src/main/java/com/psyzane/quizup/HomeActivity.java
@@ -1,24 +1,83 @@
 package com.psyzane.quizup;
 
 import android.os.Bundle;
+import android.util.Log;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private PostAdapter postAdapter;
+    private List<Post> posts = new ArrayList<>();
+    private int nextPostId = 0;
+
+    //Limiting the number of posts
+    private int totalNumberOfPosts = 100;
+    private boolean isLastPageLoaded = false;
+    private RecyclerView.OnScrollListener scrollListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        recyclerView = findViewById(R.id.postsRecyclerView);
+        postAdapter = new PostAdapter(posts);
+        recyclerView.setAdapter(postAdapter);
+
+        //Set the layout manager
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //Add initial post
+        addMorePosts(1);
+        addMorePosts(20);
+//        addMorePosts(101);
+//        addMorePosts();
+
+        //Detect end of the list
+        scrollListener = new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+                int visibleItemCount = layoutManager.getChildCount();
+                int totalItemCount = layoutManager.getItemCount();
+                int firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition();
+
+                // check if there are more posts to add
+                if (visibleItemCount + firstVisibleItemPosition >= totalItemCount && firstVisibleItemPosition >= 0 && !isLastPageLoaded) {
+                    addMorePosts(1);
+
+                    Log.i("my app", "post size" + posts.size());
+                }
+            }
+        };
+        recyclerView.addOnScrollListener(scrollListener);
+    }
+
+    private void addMorePosts(int size){
+        if(posts.size() >= totalNumberOfPosts)
+            return;
+        List<Post> newPosts = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            newPosts.add(new Post(nextPostId++, "this is a new post no. " + nextPostId));
+        }
+        postAdapter.addPost(newPosts);
+
+        // Log for post size
+        Log.i("my app", "post size on scroll" + posts.size());
+
+        //Check if there are no more posts to add
+        if (posts.size() > totalNumberOfPosts){
+            isLastPageLoaded = true;
+            recyclerView.removeOnScrollListener(scrollListener);
+        }
     }
 }
